11ded813cb6ea8da6cda0fc852d437c4e6e51d09
Shivraj Sharma <shivrajsharma21@gmail.com>
2024-07-20 11:11:38 +0530

    message

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-                         <Suspense fallback={<Loading />}>
+                           <Suspense fallback={<Loading />}>
           <ul>
             <li>
               {" "}
